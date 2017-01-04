5bbdd5fb364ea18f84d13caba05ea8d017d71800
Pierre Dérian <pderian@arrakis.home>
2017-01-04 20:18:37 +0100

    improved info, fixed couple bugs

--- decoder.py
+++ decoder.py
@@ -110,10 +110,9 @@ def read_img_preprocess_save_img(inputfile, outputfile, preprocessor, verbose=Tr
 
 def read_video_preprocess_save_img(inputfile, outputdir, preprocessor, prefix='',
                                    verbose=True, workers=None, RGB=False, cmap='gray',
-                                   format='jpg'):
+                                   format='png'):
     """
-    Read frames from the video file, preprocess and save as a PNG.
-    Note: this is a RGB version.
+    Read frames from the video file, preprocess and save as a regular image(s).
 
     Written by P. DERIAN 2016-08-17
     Updated by P. DERIAN 2017-01-04: added json info output.
@@ -123,34 +122,6 @@ def read_video_preprocess_save_img(inputfile, outputdir, preprocessor, prefix=''
         print 'File', inputfile
         print '\tloading data...'
     data = load_keyframes(inputfile, verbose=verbose)
-
-    ### json data
-    jsondata = {# metadata
-                'author': 'Pierre DERIAN',
-                'website': 'www.pierrederian.net',
-                'createdBy': __file__,
-                'description': 'Metadata for the frames extracted from video file and pre-processed. Resolution is in [m/px]; xBounds and yBounds in [m] UTM.',
-                # source data
-                'sourceVideo': os.path.basename(data['info']['file']),
-                'numberFrame': data['info']['nb_frames'],
-                # time info
-                'startTime': data['info']['creation_time'].strftime('%Y-%m-%d %H:%M:%S'),
-                'frameTimestamps': data['info']['timestamp'],
-                # domain info
-                'resolution': preprocessor.param['resolution'],
-                'xDimPx': preprocessor.x.size,
-                'yDimPx': preprocessor.y.size,
-                'xBoundsUTM': preprocessor.param['xbounds'],
-                'yBoundsUTM': preprocessor.param['ybounds'],
-                # filters
-                'medianLengthPx': preprocessor.param['median_length_px'],
-                # projection matrix
-                'projectionMatrix': preprocessor.H.tolist(),
-                }
-    jsonfile = os.path.join(outputdir, '{}info.json'.format(prefix))
-    with open(jsonfile, 'w') as f:
-        json.dump(jsondata, f, indent=0)
-    print 'wrote {}'.format(jsonfile)
 
     ### process frames
     if verbose:
@@ -168,6 +139,7 @@ def read_video_preprocess_save_img(inputfile, outputdir, preprocessor, prefix=''
             # yield the RGB or a grayscale version of the frame
             # [TODO] enable to pass custom color=>grayscale converter?
             yield ((data['frames'][n],) if RGB else (skcolor.rgb2grey(data['frames'][n]),))
+            # output progress
             if disp_progress and (not (n+1)%disp_progress):
                 print '{} {:6.2f}%  (iter {})'.format(time.ctime(), 100.*(n+1.)/N, n+1)
             n += 1
@@ -187,19 +159,57 @@ def read_video_preprocess_save_img(inputfile, outputdir, preprocessor, prefix=''
     print 'done - {:.2f} s'.format(toc-tic)
 
     ### write images
+    srcfiles = []
+    filtfiles = []
     for i, images in enumerate(processed_frames):
         img_rect, img_filt = images
+        print img_rect.dtype, img_rect.min(), img_rect.max()
+
         # save the rectified version
-        outputfile = os.path.join(outputdir, 'src_{}{:03d}.{}'.format(prefix, i, format))
-        pyplot.imsave(outputfile, img_rect, cmap=cmap)
+        basename = 'src_{}{:03d}.{}'.format(prefix, i, format)
+        outputfile = os.path.join(outputdir, basename)
+        skio.imsave(outputfile, (255.*img_rect).astype('uint8'))
         print 'saved', outputfile
+        srcfiles.append(basename)
         # save the filtered version, if any
         if img_filt is not None:
-            outputfile = os.path.join(outputdir,
-                                      'filt_{}{:03d}.{}'.format(prefix, i, format),
-                                      )
-            pyplot.imsave(outputfile, img_filt, cmap=cmap)
+            basename = 'filt_{}{:03d}.{}'.format(prefix, i, format)
+            outputfile = os.path.join(outputdir, basename)
+            skio.imsave(outputfile, (255.*img_filt).astype('uint8'))
             print 'saved', outputfile
+            filtfiles.append(basename)
+
+    ### json data
+    jsondata = {# metadata
+                'author': 'Pierre DERIAN',
+                'website': 'www.pierrederian.net',
+                'createdBy': __file__,
+                'description': 'Metadata for the frames extracted from video file and pre-processed. Resolution is in [m/px]; xBounds and yBounds in [m] UTM.',
+                # source data
+                'sourceVideo': os.path.basename(data['info']['file']),
+                'numberFrame': data['info']['nb_frames'],
+                # output data
+                'imageFormat':format,
+                'sourceImages': srcfiles,
+                'filteredImages': filtfiles,
+                # time info
+                'startTime': data['info']['creation_time'].strftime('%Y-%m-%d %H:%M:%S'),
+                'frameTimestamps': data['info']['timestamp'],
+                # domain info
+                'resolution': preprocessor.param['resolution'],
+                'xDimPx': preprocessor.x.size,
+                'yDimPx': preprocessor.y.size,
+                'xBoundsUTM': preprocessor.param['xbounds'],
+                'yBoundsUTM': preprocessor.param['ybounds'],
+                # filters
+                'medianLengthPx': preprocessor.param['median_length_px'],
+                # projection matrix
+                'projectionMatrix': preprocessor.H.tolist(),
+                }
+    jsonfile = os.path.join(outputdir, '{}info.json'.format(prefix))
+    with open(jsonfile, 'w') as f:
+        json.dump(jsondata, f, indent=0)
+    print 'wrote {}'.format(jsonfile)
 
 def read_video_preprocess_save_netcdf(inputfile, outputfile, preprocessor, verbose=True, workers=None):
     """
@@ -326,111 +336,13 @@ def read_video_preprocess_save_netcdf(inputfile, outputfile, preprocessor, verbo
     ### end
     rootgroup.close()
 
-### TEST ###
-############
-def test_image(preprocessor):
-    """
-    """
-    files = ["/Volumes/Nymeria/Science/Data/GrandPopo/20140313/06/test/1.png",
-             "/Volumes/Nymeria/Science/Data/GrandPopo/20140313/06/test/2.png",
-             ]
-    for i, f in enumerate(files):
-        output = os.path.join('tmp', '{:03d}.png'.format(i))
-        read_img_preprocess_save_img(f, output, preprocessor)
-
-def test_video(preprocessor):
-    """
-    """
-    files = ["/Volumes/Nymeria/Science/Data/GrandPopo/20140313/06/30.mp4",
-             ]
-    for f in files:
-        data = load_keyframes(f, verbose=True)
-        dt = data['info']['duration']/(data['info']['nb_frames']-1)
-        for i, frame in enumerate(data['frames']):
-            print i, i*dt
-            #img = preprocessor.grid_image(skcolor.rgb2grey(frame))
-            #skio.imsave(os.path.join('tmp', '{:03d}.png'.format(i)), img)
-    return None
-
-def test_frames_dt(preprocessor):
-
-    file = sorted(glob.glob("/Volumes/Nymeria/Science/Data/GrandPopo/20140313/16/0*.mp4"))
-    global_dt = []
-    global_time = []
-    global_info = []
-    for f in file:
-        info = video_info(f)
-        global_info.append(info)
-        t = [info['creation_time'] + datetime.timedelta(seconds=s) for s in info['timestamp']]
-        global_time += t
-        info['time'] = t
-        print t[0], '->', t[-1]
-        global_dt += [info['timestamp'][k+1]-info['timestamp'][k] for k in xrange(len(info['timestamp'])-1)]
-    print global_dt[:15]
-    nlarge = (numpy.asarray(global_dt)>0.6).sum()
-    print 'dt > 0.6: {} / {} = {}%'.format(nlarge, len(global_dt), float(nlarge)/float(len(global_dt)))
-
-    # histogram
-    pyplot.figure()
-    ax = pyplot.gca()
-    bins = numpy.linspace(0, 1., num=21)
-    ax.hist(global_dt, bins=bins)
-    ax.set_xlabel('time between frame [s]')
-    ax.set_ylabel('occurence')
-    ax.set_title('GPP Video data - distribution of video time steps\n{:%Y-%m-%d %H:%M:%S} to {:%H:%M:%S}'.format(
-        global_info[0]['time'][0], global_info[-1]['time'][-1]))
-    # plot
-    pyplot.figure()
-    ax = pyplot.gca()
-    ax.plot_date(global_time[:400], global_dt[:400], fmt='-b')
-    ax.set_xlabel('time')
-    ax.set_ylabel('time step [s]')
-    ax.xaxis.set_major_formatter(dates.DateFormatter('%H:%M:%S'))
-    ax.set_title('GPP Video data - time steps\nstarting {:%Y-%m-%d %H:%M}'.format(
-        global_info[0]['time'][0]))
-    pyplot.show()
-
-def test_netcdf(inputfile, outputdir, raw=False):
-    """
-    Check the content of a netcdf file of image data
-    as created by read_video_preprocess_save_netcdf().
-
-    Arguments:
-        inputfile the input netcdf archive
-        outputdir a directory for ouput images
-
-    Written by P. DERIAN 2016-03-09
-    """
-    # load variables of interest
-    rootgroup = netCDF4.Dataset(inputfile, 'r', format='NETCDF4')
-    img = rootgroup.variables['img'][:]
-    x = rootgroup.variables['x'][:]
-    y = rootgroup.variables['y'][:]
-    t0 = netCDF4.num2date(rootgroup.variables['t'][0], rootgroup.variables['t'].units)
-    dt = rootgroup.variables['dt'][:]
-    rootgroup.close()
-    # display each frame
-    for i in xrange(img.shape[0]):
-        # restore frame datetime
-        t = t0 + datetime.timedelta(seconds=float(dt[i]))
-        if raw:
-            outfile = outputdir+'_{:03d}.jpg'.format(i)
-            pyplot.imsave(outfile, img[i], cmap='gray', vmin=0, vmax=1)
-            print 'saved', outfile
-        else:
-            fig = pyplot.figure()
-            ax = pyplot.axes(title=str(t), xlabel='UTM eastern [m]', ylabel='UTM northern [m]')
-            ax.imshow(img[i], extent=[x[0], x[-1], y[-1], y[0]], interpolation='none', cmap='gray')
-            fig.savefig(os.path.join(outputdir, '{:03d}.png'.format(i)))
-            pyplot.close(fig)
-
 ### MAIN ####
 #############
 def main(argv):
     # Arguments
     parser = argparse.ArgumentParser()
     parser.add_argument("-o", "--output", default=None, dest="outputfile", help="netcdf output file / image output directory (with -i, --img)")
-    parser.add_argument("-r", "--radon", default=0., type=float, dest="radon_length", help="length of radon filter in [m]")
+    parser.add_argument("-r", "--radon", default=0., type=float, dest="radon_length", help="[DEPRECATED] length of radon filter in [m]")
     parser.add_argument("-m", "--median", default=0., type=float, dest="median_length", help="length of median filter in [m]")
     parser.add_argument("--img", dest="asImg", action="store_true", help="export images")
     parser.add_argument("--nc", dest="asImg", action="store_false", help="export NetCDF (legacy)")
@@ -444,7 +356,7 @@ def main(argv):
     y_UTM = 694098
     #x_UTM = 370260 #flash rip
     #y_UTM = 694090
-    dim = 30.
+    dim = 60.
     resolution = .1 #sensors
     #resolution = .2 #flash rip
     radon_length = args.radon_length
@@ -477,9 +389,6 @@ def main(argv):
             args.outputfile,
             preproc,
         )
-
-    #p, _ =  os.path.splitext(args.outputfile)
-    #test_netcdf(args.outputfile, p, True)
 
 if __name__=="__main__":
     main(sys.argv[1:])
